97105831361a3ad2a0338d4c65bbbfee0f6a453f
Sylvain <sylvain.gaultier@telecom-sudparis.eu>
2019-09-01 15:18:50 +0200

    update example

--- example/tof_sensor/tof_sensor.ino
+++ example/tof_sensor/tof_sensor.ino
@@ -7,26 +7,20 @@
 #include <Wire.h>
 #include "ToF_sensor.h"
 
-#define PIN_STANDBY 2
-#define MIN_DISTANCE 20
-#define MAX_DISTANCE 800
-
-ToF_shortRange sensor1;
-ToF_shortRange sensor2(42, 13);
-ToF_shortRange sensor3(43, 14, 20, 150, "sensor", &Serial);
-
-ToF_longRange_med<3> sensor4;
-ToF_longRange_med<3> sensor5(42, 13);
-ToF_longRange_med<3> sensor6(43, 14, 20, 150, "sensor", &Serial);
+ToF_shortRange sensor1; // Uninitialized sensor (will do nothing)
+ToF_shortRange sensor2(42, 13); // Default short range sensor with address 42, on pin 13
+ToF_shortRange sensor3(43, 14, 20, 150, "sensor", &Serial); // Named sensor with a Stream to output logs
+ToF_longRange sensor4(44, 15); // Default long range sensor
+ToF_longRange_med<3> sensor5(45, 16); // Default long range sensor with median filter
 
 void setup()
 {
+	Wire.begin();
     sensor1.powerON();
     sensor2.powerON();
     sensor3.powerON();
     sensor4.powerON();
     sensor5.powerON();
-    sensor6.powerON();
 }
 
 void loop()
@@ -36,6 +30,6 @@ void loop()
     Serial.println(sensor3.getMeasure());
     Serial.println(sensor4.getMeasure());
     Serial.println(sensor5.getMeasure());
-    Serial.println(sensor6.getMeasure());
     delay(100);
 }
+
